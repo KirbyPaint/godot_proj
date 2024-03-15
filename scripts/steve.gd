8ca88335d25dc969752359634774076ad591e97c
KirbyPaint <porter.ashley13@gmail.com>
2024-03-14 22:08:04 -0700

    updates

--- scripts/steve.gd
+++ scripts/steve.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 var playerName = "Kerby"
 # put consts in ALL CAPS
-const SPEED = 20
+const SPEED = 14
+const ROTATION_SPEED = deg_to_rad(8.0)
 # var characterVelocity = Vector3(0,0,0)
 
 # Called when the node enters the scene tree for the first time.
@@ -16,8 +17,10 @@ func _physics_process(_delta):
 		velocity.x = 0
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
+		rotate_z(-ROTATION_SPEED)
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -SPEED
+		rotate_z(ROTATION_SPEED)
 	else:
 		# lerp() throws errors when not all arguments are the same
 		# lerpf() somehow converts all to floats I believe
@@ -27,12 +30,13 @@ func _physics_process(_delta):
 		velocity.z = 0
 	elif Input.is_action_pressed("ui_up"):
 		velocity.z = -SPEED
+		rotate_x(-ROTATION_SPEED)
 	elif Input.is_action_pressed("ui_down"):
 		velocity.z = SPEED
+		rotate_x(ROTATION_SPEED)
 	else:
 		velocity.z = lerpf(velocity.z,0,0.1)
 	move_and_slide()
-	print(velocity)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
